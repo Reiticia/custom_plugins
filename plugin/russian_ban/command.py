a9e4b94451c0b2ddd4a703e4c1290d0441d1642a
reine-ishyanami <2402979195@qq.com>
2024-07-19 10:04:51 +0800

    refactor: extract rule to rule.py

--- plugin/russian_ban/command.py
+++ plugin/russian_ban/command.py
@@ -1,6 +1,7 @@
 import re
 from .schedule import save_mute, add_schedule, remove_schedule, muted_list_dict, schedule_dict
-from .decorator import switch_depend, mute_sb_stop_runpreprocessor
+from .decorator import switch_depend, mute_sb_stop_runpreprocessor, negate_return_value
+from .rule import check_mute_sb, check_mute_sb_p_at_st
 from nonebot import on_command, logger
 from nonebot.matcher import Matcher
 from nonebot.permission import SUPERUSER
@@ -58,8 +59,8 @@ async def save_user_id_nickname(event: GroupMessageEvent):
 
 
 @run_preprocessor
-@switch_depend(dependOn=[lambda: switch], ignoreIds=ignoreIds)
-async def random_mute(bot: Bot, event: GroupMessageEvent):
+@switch_depend(dependOn=[lambda *args, **kwargs: switch, negate_return_value(check_mute_sb)], ignoreIds=ignoreIds)
+async def random_mute(bot: Bot, event: GroupMessageEvent, cs: str = CommandStart()):
     """触发指令时，对某人进行随机禁言
 
     Args:
@@ -187,29 +188,8 @@ def dict_group_by_group_id(members: dict[str, dict[str, int]]) -> dict[str, dict
     return res
 
 
-def add_user_ignore(
-    event: GroupMessageEvent, cs: str = CommandStart(), cmd: tuple[str, ...] = ("mute sb", "msb")
-) -> bool:
-    """判断事件是否为 mute sb 命令，如果是，则将 user_id 添加到 ignoreIds 中
 
-    Args:
-        event (GroupMessageEvent): 群组消息事件
-
-    Returns:
-        bool: 匹配是否成功
-    """
-    logger.debug(f"cs: {cs}")
-    if not (msg := event.message.extract_plain_text().strip()).startswith(cs):
-        return False
-    logger.debug(f"cmd: {cmd}")
-    if msg.removeprefix(cs).strip() not in cmd:
-        return False
-    logger.debug(f"{ignoreIds} will append {event.user_id}")
-    ignoreIds.add(event.user_id)
-    return True
-
-
-mute_sb_cmd = on_command(cmd="mute sb", rule=add_user_ignore, aliases={"msb"})
+mute_sb_cmd = on_command(cmd="mute sb", aliases={"msb"})
 
 
 @mute_sb_cmd.handle()
@@ -410,45 +390,7 @@ def at_members(members: set[int]) -> Message:
     """
     return Message([MessageSegment.at(member) for member in members])
 
-
-def mute_sb_p_at_st(
-    event: GroupMessageEvent, cs: str = CommandStart(), cmd: tuple[str, ...] = ("mute schedule", "ms")
-) -> bool:
-    """在某时某分让某人被禁言某段时间
-
-    Args:
-        event (GroupMessageEvent): 消息事件
-
-    Returns:
-        bool: 是否匹配
-    """
-    message = event.get_message()
-    if len(message) != 3:
-        return False
-
-    text_0: str = message[0].data.get("text", "").strip()
-    # 判断指令前缀
-    if not text_0.startswith(cs):
-        return False
-
-    # 判断指令
-    for c in cmd:
-        if text_0.removeprefix(cs).strip().startswith(c):
-            break
-    else:
-        return False
-
-    if message[1].type != "at" or message[2].type != "text":
-        return False
-
-    text_2 = message[2].data.get("text", "").strip()
-    pattern = r"^(\d+)\s*at\s*([01]?\d|2[0-3])(:[0-5]?\d)?$"
-    if re.match(pattern, text_2):
-        return True
-    return False
-
-
-mute_schedule_cmd = on_command(cmd="mute schedule", aliases={"ms"}, rule=mute_sb_p_at_st, permission=permit_roles)
+mute_schedule_cmd = on_command(cmd="mute schedule", aliases={"ms"}, rule=check_mute_sb_p_at_st, permission=permit_roles)
 
 
 def split_event_args(msg: Message) -> tuple[int, int, str, str]:
