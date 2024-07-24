c25eed6cfd3dd7af99f64438d0186857bfc309d6
reine-ishyanami <2402979195@qq.com>
2024-07-24 10:24:17 +0800

    feat: orm

--- plugin/ban_image/__init__.py
+++ plugin/ban_image/__init__.py
@@ -7,9 +7,10 @@ from nonebot.adapters.onebot.v11.event import Reply
 from typing import Optional
 from nonebot.matcher import Matcher
 from .config import Config
-from .model import BanImage
+from .struct import BanImage
+from nonebot.params import Depends
 
-ban_image = BanImage()
+ban_images: dict[int, BanImage] = {}
 
 __plugin_meta__ = PluginMetadata(
     name="ban_image",
@@ -26,7 +27,26 @@ permit_roles = GROUP_OWNER | SUPERUSER | GROUP_ADMIN
 """
 
 
-async def check_img(event: GroupMessageEvent):
+async def get_ban_image(event: GroupMessageEvent) -> BanImage:
+    """子依赖 获取对应群组的BanImage信息
+
+    Args:
+        event (GroupMessageEvent): 群组消息事件
+
+    Returns:
+        BanImage: 违禁图片
+    """
+    global ban_images
+    if (ban_image := ban_images.get(group_id := event.group_id)) is not None:
+        return ban_image
+    else:
+        ban_images.update({
+            group_id: (ban_image := await BanImage(group_id).load())
+        })
+        return ban_image
+
+
+async def check_img(event: GroupMessageEvent, ban_image: BanImage = Depends(get_ban_image)):
     """判断是否违禁图片"""
     img_message = event.get_message().include("image")
     if len(img_message) == 0:
@@ -40,11 +60,12 @@ async def check_img(event: GroupMessageEvent):
 async def ban_img_sender(bot: Bot, event: GroupMessageEvent, matcher: Matcher):
     """违禁图片禁言"""
     await bot.delete_msg(message_id=event.message_id)
-    await bot.set_group_ban(group_id=event.group_id, user_id=event.user_id, duration=60)
+    user_id = event.user_id
+    await bot.set_group_ban(group_id=event.group_id, user_id=user_id, duration=60)
     await matcher.finish(" 听不懂人话？不是叫你别发了吗？", at_sender=True)
 
 
-async def check_message_add(event: GroupMessageEvent):
+async def check_message_add(event: GroupMessageEvent, ban_image: BanImage = Depends(get_ban_image)):
     """检测消息合法性"""
     cmd_msg = [msg.data.get("text", "").strip() for msg in event.get_message() if msg.type == "text"]
     if "别发了" in cmd_msg:
@@ -52,7 +73,6 @@ async def check_message_add(event: GroupMessageEvent):
         if reply_msg and len(imgs := reply_msg.message.include("image")) > 0:
             # 添加到违禁图片列表
             await ban_image.add_ban_image([img for img in imgs])
-            await ban_image.save()
             return True
     return False
 
@@ -66,7 +86,7 @@ async def add_ban_image(event: GroupMessageEvent, matcher: Matcher):
     await matcher.finish(message=message)
 
 
-async def check_message_del(event: GroupMessageEvent):
+async def check_message_del(event: GroupMessageEvent, ban_image: BanImage = Depends(get_ban_image)):
     """检测消息合法性"""
     cmd_msg = [msg.data.get("text", "").strip() for msg in event.get_message() if msg.type == "text"]
     if "随便发" in cmd_msg:
@@ -74,7 +94,6 @@ async def check_message_del(event: GroupMessageEvent):
         if reply_msg and len(imgs := reply_msg.message.include("image")) > 0:
             # 删除指定违禁图片
             await ban_image.remove_ban_image([img for img in imgs])
-            await ban_image.save()
             return True
     return False
 
@@ -86,7 +105,7 @@ async def remove_ban_image(matcher: Matcher):
 
 
 @on_fullmatch(msg="让我看看什么不能发").handle()
-async def list_ban_images(bot: Bot, event: GroupMessageEvent):
+async def list_ban_images(bot: Bot, event: GroupMessageEvent, ban_image: BanImage = Depends(get_ban_image)):
     message = await ban_image.list_ban_image("bot", bot.self_id)
     if len(message) > 0:
         await bot.call_api(
---
+++ plugin/ban_image/struct.py
@@ -0,0 +1,209 @@
+from os import makedirs, remove
+from pathlib import Path
+import time
+from typing import Any, Optional
+from aiofiles import open as aopen
+from nonebot.adapters.onebot.v11 import MessageSegment, Message
+from httpx import AsyncClient
+from nonebot_plugin_orm import get_session
+from sqlalchemy.future import select
+from sqlalchemy import delete
+from .model import GroupImageBanInfo
+
+from nonebot import require
+
+require("nonebot_plugin_localstore")
+
+import nonebot_plugin_localstore as store  # noqa: E402
+
+
+class BanImage:
+    def __init__(self, group_id: int) -> None:
+        self.group_id = group_id
+        """群组id"""
+        self.img_store: Path = store.get_data_dir("ban_image").joinpath(f"img_store_{group_id}")
+        """图片存储位置"""
+
+    async def load(self) -> "BanImage":
+        session = get_session()
+        async with session.begin():
+            # 过滤指定群组所有违禁图片信息
+            condition = select(GroupImageBanInfo).filter_by(group_id=self.group_id)
+            infos: list[GroupImageBanInfo] = (await session.execute(condition)).scalars().all()
+            self.sizes: dict[int, str] = {info.file_size: info.img_name for info in infos}
+            """存储图片尺寸及图片名称"""
+        return self
+
+    async def add_ban_image(self, imgs: list[MessageSegment]):
+        """添加禁言图片
+
+        Args:
+            img (list[MessageSegment]): 追加的禁言图片
+        """
+        if not self.img_store.exists():
+            makedirs(self.img_store.as_posix())
+        gibis = []
+        async with AsyncClient() as client:
+            for img in imgs:
+                img_name = img.data.get("file")
+                url = img.data.get("url")
+                size = int(img.data.get("file_size"))
+                # 构造数据
+                gibi = GroupImageBanInfo(group_id=self.group_id, file_size=size, img_name=img_name)
+                gibis.append(gibi)
+                # 下载图片到本地
+                response = await client.get(url)
+                async with aopen(self.img_store.joinpath(img_name), "wb") as f:
+                    await f.write(response.content)
+        # 保存到数据库中
+        session = get_session()
+        async with session.begin():
+            session.add_all(gibis)
+        await session.commit()
+        await self.load()
+
+
+    async def remove_ban_image(self, imgs: list[MessageSegment]):
+        """删除禁言图片
+
+        Args:
+            img (list[MessageSegment]): 删除的禁言图片
+        """
+        if not self.img_store.exists():
+            makedirs(self.img_store.as_posix())
+        # 删除数据库对应信息
+        sizes = set([img.data.get("file_size") for img in imgs])
+        session = get_session()
+        async with session.begin():
+            await session.execute(delete(GroupImageBanInfo).where(GroupImageBanInfo.file_size.in_(sizes)))
+        await session.commit()
+        self.load()
+        for img in imgs:
+            # 添加文件大小到缓冲区
+            file_size = img.data.get("file_size")
+            # 删除本地图片
+            file = self.img_store.joinpath(str(self.sizes.get(file_size)))
+            try:
+                remove(file)
+            except FileNotFoundError:
+                print(f"文件 {file} 不存在。")
+
+    async def list_ban_image(self, name: str, uid: int) -> list:
+        """展示已被禁止的图片
+
+        Args:
+            name(str): bot昵称
+            uid(int): bot账号
+
+        Returns:
+            list: 消息
+        """
+        msg = []
+        for size, name in self.sizes.items():
+            async with aopen(self.img_store.joinpath(name), "rb") as f:
+                content = await f.read()
+                ms1 = MessageSegment.text(text=size)
+                ms2 = MessageSegment.image(file=content)
+                msg.append(
+                    {
+                        "type": "node",
+                        "data": {
+                            "name": name,
+                            "uin": uid,
+                            "content": Message([ms1, ms2]),
+                        },
+                    }
+                )
+        return msg
+
+    def check_image_equals(self, img: MessageSegment) -> bool:
+        """检测是否匹配 TODO 更好的匹配策略
+
+        Args:
+            img (MessageSegment): 图片消息段
+
+        Return:
+            bool: 是否匹配
+        """
+        return int(img.data.get("file_size")) in self.sizes.keys()
+
+
+class ExpirableDict:
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.data: dict[str, Any] = {}
+        self.expiry: dict[str, int] = {}
+
+    def set(self, key: str, value: Any, ttl: Optional[int] = None) -> None:
+        self.data[key] = value
+        # 如果有设置过期时间，则更新过期时间
+        if ttl is not None:
+            expiry_time = int(time.time()) + ttl
+            self.expiry[key] = expiry_time
+
+    def get(self, key: str) -> Optional[Any]:
+        # 如果没有设置过期时间，则直接返回值
+        if self.expiry.get(key) is None:
+            return self.data.get(key)
+        # 如果有过期时间，且已过期，则删除
+        if int(time.time()) > self.expiry[key]:
+            del self.data[key]
+            del self.expiry[key]
+        return self.data.get(key)
+
+    def delete(self, key: str) -> None:
+        if key in self.data:
+            del self.data[key]
+        if key in self.expiry:
+            del self.expiry[key]
+
+    def exists(self, key: str) -> bool:
+        return self.get(key) is not None
+
+    def __add__(self, other: "ExpirableDict") -> "ExpirableDict":
+        result = ExpirableDict(name=self.name)
+        result.data = {k: v.copy() for k, v in self.data.items()}
+        result.expiry = {k: v.copy() for k, v in self.expiry.items()}
+
+        for key, value in other.data.items():
+            if key not in result.data:
+                result.data[key] = value
+
+        for key, value in other.expiry.items():
+            if key not in result.expiry:
+                result.expiry[key] = value
+
+        return result
+
+    def __sub__(self, other: "ExpirableDict") -> "ExpirableDict":
+        result = ExpirableDict(name=self.name)
+        result.data = {k: v.copy() for k, v in self.data.items()}
+        result.expiry = {k: v.copy() for k, v in self.expiry.items()}
+
+        for key, _ in other.data.items():
+            if key in result.data:
+                del result.data[key]
+            if key in result.expiry:
+                del result.expiry[key]
+
+        return result
+
+    def __repr__(self) -> str:
+        res = f"{ExpirableDict.__name__}: {self.name}"
+        del_key = []
+        for key, value in self.data.items():
+            # 过期时间为None，表示永不过期
+            if (expiry := self.expiry.get(key)) is None:
+                ttl = -1
+            else:
+                # 如果过期
+                if (ttl := expiry - int(time.time())) <= 0:
+                    del_key.append(key)
+                    continue
+            line = f"\n{key}\t{value}\t{ttl}"
+            res += line
+
+        for key in del_key:
+            self.delete(key)
+
+        return res
