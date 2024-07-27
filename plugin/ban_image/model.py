23f4ef7ee60c20a797a004e33d69e8fac81a580d
reine-ishyanami <2402979195@qq.com>
2024-07-27 10:42:19 +0800

    feat: add index of GroupImageBanInfo

--- plugin/ban_image/model.py
+++ plugin/ban_image/model.py
@@ -7,7 +7,7 @@ class GroupImageBanInfo(Model):
     """群组违禁图片信息"""
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     """主键"""
-    group_id: Mapped[int]
+    group_id: Mapped[int] = mapped_column(index=True)
     """群组id"""
     file_size: Mapped[str]
     """文件大小"""
