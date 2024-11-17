87f753326ca64c69424a35fbaf3f6caaa281bb6f
reine-ishyanami <2402979195@qq.com>
2024-11-17 20:46:58 +0800

    fix(approve_friend)

--- plugins/approve_friend/__init__.py
+++ plugins/approve_friend/__init__.py
@@ -1,4 +1,4 @@
-from nonebot import get_plugin_config, on
+from nonebot import get_plugin_config, on, logger, on_request
 from nonebot.plugin import PluginMetadata
 from nonebot.adapters.onebot.v11 import FriendRequestEvent, Bot
 
@@ -14,8 +14,9 @@ __plugin_meta__ = PluginMetadata(
 config = get_plugin_config(Config)
 
 
-@on(type="request.friend").handle()
+@on_request().handle()
 async def _(bot: Bot, e: FriendRequestEvent):
+    logger.info(f"收到好友请求：{e.user_id}")
     await e.approve(bot=bot)
     for user in bot.config.superusers:
         if user == bot.self_id:
