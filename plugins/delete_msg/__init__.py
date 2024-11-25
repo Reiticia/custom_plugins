1ac891d2627c0658fee35069ed325ab6dfd1800c
reine-ishyanami <2402979195@qq.com>
2024-11-25 17:50:57 +0800

    fix: sender permission fix

--- plugins/delete_msg/__init__.py
+++ plugins/delete_msg/__init__.py
@@ -21,16 +21,22 @@ async def _(bot: Bot, event: GroupMessageEvent):
     reply = event.reply
     if not reply:
         await delete_msg.finish("你好歹告诉我删啥吧")
-    # 权限认证
+    # 发送方权限认证
     if (
-        str(event.user_id) not in bot.config.superusers
+        str(event.user_id) not in bot.config.superusers  # 如果不是超级用户
         and event.user_id != reply.sender.user_id
-        and event.sender.role == "member"
+        and (
+            event.sender.role == "member"  # 普通成员意图撤回其他人的消息
+            or event.sender.role == reply.sender.role == "admin"  # 管理员意图撤回其他管理员的消息
+        )
     ):
-        await delete_msg.finish("你只能删你自己的消息")
+        await delete_msg.finish("你的权限不足以删除该消息")
     bot_info = await bot.get_group_member_info(group_id=event.group_id, user_id=event.self_id)
+    # 机器人权限认证
     if bot_info["role"] == "member" or (
-        bot_info["role"] == "admin" and reply.sender.role in ["owner", "admin"] and str(reply.sender.user_id) != bot.self_id
+        bot_info["role"] == "admin"
+        and reply.sender.role in ["owner", "admin"]
+        and str(reply.sender.user_id) != bot.self_id
     ):
         await delete_msg.finish("我是废物，你找别人帮你撤回吧")
     await bot.delete_msg(message_id=reply.message_id)
